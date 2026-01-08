512591caf6fd5ad7349b4af57d66492ccb47d882
IanL73 <ianl73@nycstudents.net>
2026-01-08 13:56:15 -0500

    yeah no way I'm finishing this on time

--- app/src/main.js
+++ app/src/main.js
@@ -8,7 +8,8 @@ if (EveryPokemonEverResponse.status != 200) {
   throw new Error("failed to fetch data :(")
 }
 else {
-  const EveryPokemonEver = await EveryPokemonEverResponse.json()
+
+  const EveryPokemonEver = await EveryPokemonEverResponse.json()  // Get all the data we need
   let response1number = getRandomInt(1350)
   const response1 = await fetch(EveryPokemonEver.results[response1number].url)
   const card1 = document.querySelector(".card1");
@@ -21,47 +22,59 @@ else {
   if (response1.status !== 200 & response2.status !== 200){
     throw new Error("Failed to fetch data :(")
   }
+
   else {
+    while(true) {
     console.log(response1)
     console.log(response2)
     const data1 = await response1.json()
     const data2 = await response2.json()
     console.log(data1)
     console.log(data2)
-    const name1 = data  // WORKING ON THIS DON'T IGNORE >:(
+    const name1 = data1.name  // Create card 1
     const image1 = data1.sprites.front_default
     console.log(card1)
     card1.insertAdjacentHTML(
         "beforeend",
-        `<h3>${image}</h3>`)
-    console.log(data1)
-    document.querySelector(".card1").addEventListener("click", () => {
+        `
+        <h3>${name1}</h3>
+        <img src="${image1}"><img/>
+        `)
+
+    document.querySelector(".card1").addEventListener("click", () => {  // Tell you weather card 1 or two is wrong
     if (data1.stats[1].base_stat > data2.stats[1].base_stat) {
-      document.querySelector(".container").insertAdjacentHTML(
-        "beforeend",
+      document.querySelector(".outcome").innerHTML = ""
+      document.querySelector(".outcome").insertAdjacentHTML(
+        "afterbegin",
         `<h2>CORRECT</h2>`)
     }else{
-      document.querySelector(".container").insertAdjacentHTML(
-        "beforeend",
+      document.querySelector(".outcome").insertAdjacentHTML(
+        "afterbegin",
         `<h2>WRONG</h2>`)
-    }
-    })
+    }})
+
+
+    const name2 = data2.name
     const image2 = data2.sprites.front_default
     console.log(card2)
     card2.insertAdjacentHTML(
         "beforeend",
-        `<img src="${image2}"/>`
+        `
+        <h3>${name2}</h3>
+        <img src="${image2}"/>
+        `
     )
     document.querySelector(".card2").addEventListener("click", () => {
     if (data1.stats[1].base_stat < data2.stats[1].base_stat) {
-      document.querySelector(".container").insertAdjacentHTML(
-        "beforeend",
+      document.querySelector(".outcome").innerHTML = ""
+      document.querySelector(".outcome").insertAdjacentHTML(
+        "afterbegin",
         `<h2>CORRECT</h2>`)
     }else{
-      document.querySelector(".container").insertAdjacentHTML(
-        "beforeend",
+      document.querySelector(".outcome").insertAdjacentHTML(
+        "afterbegin",
         `<h2>WRONG</h2>`)
     }
-    })
+    }}
   }
 }
